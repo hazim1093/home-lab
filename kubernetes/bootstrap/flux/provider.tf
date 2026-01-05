cfd9ec7a4a9133f41f2152113865feec0c4ccd8d
Hazim <hazim_malik@hotmail.com>
2026-01-05 10:42:41 +0100

    chore(deps): update terraform github to ~> 6.9.0 (#25)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- kubernetes/bootstrap/flux/provider.tf
+++ kubernetes/bootstrap/flux/provider.tf
@@ -16,7 +16,7 @@ terraform {
     }
     github = {
       source  = "integrations/github"
-      version = "~> 6.4.0"
+      version = "~> 6.9.0"
     }
     tls = {
       source  = "hashicorp/tls"
